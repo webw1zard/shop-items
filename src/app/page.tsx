930b7e1e272a750bff875f48e607c7e317622f5d
f0rest <ulugbeknizomov999@gmail.com>
2025-03-08 16:22:42 +0500

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import React from "react";
 import Product from "./(product)/page";
 import Section from "@/components/Section";
 
